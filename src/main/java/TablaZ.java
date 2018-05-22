80308782fe0f174335bf7a0bacc06e4f06b1ff4f
Claudia <Claudia@LAPTOP-GAU5GIB0>
2018-05-22 12:30:03 -0400

    ayuuda

--- src/main/java/TablaZ.java
+++ src/main/java/TablaZ.java
@@ -9,37 +9,20 @@
  * @author Ce
  */
 public class TablaZ {
-<<<<<<< HEAD
-=======
 
-
->>>>>>> origin/master
     int[][] tabla;
-<<<<<<< HEAD
 
-    public int[][] sumaTabla(int x, String operacion) {
-        tabla = new int[x][x];
-        for (int i = 0; i < x; i++) {
-            
-            for (int j = 0; j < x; j++) {
-                tabla[i][j]= i%j;        
-                        }
-        }
-        return tabla;
-=======
     
     public void operacionTabla(int n, String op){
         if(op.equals("+"));
             sumaTabla(n);
         if(op.equals("*")); 
             multiplicacion(n);
->>>>>>> origin/master
+
     }
 
-<<<<<<< HEAD
-    public void multiplicación(int n){
-        tabla= new int[n][n];
-=======
+
+
     public int[][] sumaTabla(int x) {
         tabla = new int[x][x];
         for (int i = 0; i < x; i++) {
@@ -52,19 +35,16 @@ public class TablaZ {
         return tabla;
     }
 
-    private int[][] matriz;
+  
    
     public void multiplicacion(int n){
-        matriz= new int[n][n];
->>>>>>> origin/master
+        tabla= new int[n][n];
+
         for(int i=0;i<n;i++){
             for(int c=0;c<n;c++){
                 tabla[i][n]=n*i;
             }
         }
     }
-<<<<<<< HEAD
-=======
 
->>>>>>> origin/master
 }
